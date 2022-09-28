ee7e8b3ff67663f974ca498f37856a805565c0a4
vladimir <slr-klesks-300@yandex.ru>
2022-09-28 13:32:17 +1000

    домашняя работа

---
+++ Task2/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение не возможно преобразовать в число");
+}
+
+int[,] GenerateArray(int length, int minRange, int maxRange)
+{
+    var array = new int[length, length];
+    var random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(minRange, maxRange + 1);
+
+        }
+    }
+    return array;
+}
+
+int[] FindElement(int[,] inputMatrix, int searchElement, int numberEntry)
+{
+    int[] answer = new int[2];
+    int count = 0;
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            if (inputMatrix[i, j] == searchElement)
+            {
+                answer[0] = i;
+                answer[1] = j;
+                count++;
+                if (numberEntry == count)
+                {
+                    return answer;
+                }
+            }
+        }
+
+    }
+    return answer;
+}
+
+bool IsFound(int[] inputArray)
+{
+    return !(inputArray[0] == 0 && inputArray[1] == 0);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] numbers = GenerateArray(4, 0, 10);
+PrintArray(numbers);
+int searchNum = Prompt("Введите искомое число > ");
+int[] answer = FindElement(numbers, searchNum, 2);
+if (!IsFound(answer))
+{
+    System.Console.WriteLine("такого элемента нет.");
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int row = 7;
+int col = 5;
+int min = 1;
+int max = 10;
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 10) System.Console.Write($"{array[i, j]}\t");
+            else System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] GetArithmeticMean(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+double[] arithmeticMean = GetArithmeticMean(matrix);
+PrintArray(arithmeticMean);
+System.Console.WriteLine();
+System.Console.WriteLine();
